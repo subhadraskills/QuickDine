396ca662d65fb4fc6ffd015d96e994aa9fb1f8a0
Subhadra Kumari <subhadraskills@gmail.com>
2026-07-26 16:03:01 +0530

    your commit message

--- server/routes/bookingRoutes.ts
+++ server/routes/bookingRoutes.ts
@@ -5,7 +5,7 @@ import { cancelMyBooking, createBooking, getMyBooking } from "../controllers/boo
 const bookingRouter= Router();
 
 bookingRouter.post("/", protect, createBooking)
-bookingRouter.post("/my", protect, getMyBooking)
+bookingRouter.get("/my", protect, getMyBooking)
 bookingRouter.post("/:id/cancel", protect, cancelMyBooking)
 
 export default bookingRouter;
